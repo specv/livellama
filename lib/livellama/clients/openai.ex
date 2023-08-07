83c83443f3f877222d646b516cce266ff3cf7cee
specv <61646426+specv@users.noreply.github.com>
2023-08-07 23:50:26 +0800

    `mix format`

--- lib/livellama/clients/openai.ex
+++ lib/livellama/clients/openai.ex
@@ -1,49 +1,49 @@
 defmodule LiveLlama.OpenAI do
-  @opts NimbleOptions.new! [
-    model: [
-      type: :string,
-      required: true,
-      doc: "ID of the model to use."
-    ],
-    messages: [
-      type: {:list, {:map, :string, :string}},
-      required: true,
-      doc: "A list of messages comprising the conversation so far."
-    ],
-    stream: [
-      type: :boolean,
-      default: true,
-      doc: """
-      If set, partial message deltas will be sent, like in ChatGPT.
-      Tokens will be sent as data-only server-sent events as they become available,
-      with the stream terminated by a `data: [DONE]` message.
-      """
-    ],
-    temperature: [
-      type: :float,
-      doc: """
-      What sampling temperature to use, between 0 and 2.
-      Higher values like 0.8 will make the output more random,
-      while lower values like 0.2 will make it more focused and deterministic.
+  @opts NimbleOptions.new!(
+          model: [
+            type: :string,
+            required: true,
+            doc: "ID of the model to use."
+          ],
+          messages: [
+            type: {:list, {:map, :string, :string}},
+            required: true,
+            doc: "A list of messages comprising the conversation so far."
+          ],
+          stream: [
+            type: :boolean,
+            default: true,
+            doc: """
+            If set, partial message deltas will be sent, like in ChatGPT.
+            Tokens will be sent as data-only server-sent events as they become available,
+            with the stream terminated by a `data: [DONE]` message.
+            """
+          ],
+          temperature: [
+            type: :float,
+            doc: """
+            What sampling temperature to use, between 0 and 2.
+            Higher values like 0.8 will make the output more random,
+            while lower values like 0.2 will make it more focused and deterministic.
 
-      We generally recommend altering this or `top_p` but not both.
-      """
-    ],
-    top_p: [
-      type: :float,
-      doc: """
-      An alternative to sampling with temperature, called nucleus sampling,
-      where the model considers the results of the tokens with top_p probability mass.
-      So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+            We generally recommend altering this or `top_p` but not both.
+            """
+          ],
+          top_p: [
+            type: :float,
+            doc: """
+            An alternative to sampling with temperature, called nucleus sampling,
+            where the model considers the results of the tokens with top_p probability mass.
+            So 0.1 means only the tokens comprising the top 10% probability mass are considered.
 
-      We generally recommend altering this or `temperature` but not both.
-      """
-    ],
-    api_key: [
-      type: :string,
-      required: true
-    ]
-  ]
+            We generally recommend altering this or `temperature` but not both.
+            """
+          ],
+          api_key: [
+            type: :string,
+            required: true
+          ]
+        )
   @doc "Supported options:\n#{NimbleOptions.docs(@opts)}"
   def chat_completions(opts) do
     opts = NimbleOptions.validate!(opts, @opts)
@@ -68,17 +68,19 @@ defmodule LiveLlama.OpenAI do
   end
 
   defp stream_request!(request, stream_to, ref) do
-    Req.request!(request, finch_request: fn request, finch_request, finch_name, finch_options ->
-      on_chunk = fn chunk, response ->
-        send(stream_to, {ref, chunk})
-        response
-      end
+    Req.request!(request,
+      finch_request: fn request, finch_request, finch_name, finch_options ->
+        on_chunk = fn chunk, response ->
+          send(stream_to, {ref, chunk})
+          response
+        end
 
-      case Finch.stream(finch_request, finch_name, Req.Response.new(), on_chunk, finch_options) do
-        {:ok, response} -> {request, response}
-        {:error, exception} -> {request, exception}
+        case Finch.stream(finch_request, finch_name, Req.Response.new(), on_chunk, finch_options) do
+          {:ok, response} -> {request, response}
+          {:error, exception} -> {request, exception}
+        end
       end
-    end)
+    )
 
     send(stream_to, {ref, :done})
   end
@@ -86,23 +88,27 @@ defmodule LiveLlama.OpenAI do
   defp stream_response!(request) do
     {stream_to, ref} = {self(), make_ref()}
     task = Task.async(fn -> stream_request!(request, stream_to, ref) end)
+
     Req.Response.new(
-      status: receive do
-        {^ref, {:status, status}} -> status
-      end,
-      hedaers: receive do
-        {^ref, {:headers, headers}} -> headers
-      end,
-      body: Stream.resource(
-        fn -> {ref, task} end,
-        fn {ref, task} ->
-          receive do
-            {^ref, {:data, chunk}} -> {parse_chunk(chunk), {ref, task}}
-            {^ref, :done} -> {:halt, {ref, task}}
-          end
+      status:
+        receive do
+          {^ref, {:status, status}} -> status
         end,
-        fn {_ref, task} -> Task.shutdown(task) end
-      )
+      hedaers:
+        receive do
+          {^ref, {:headers, headers}} -> headers
+        end,
+      body:
+        Stream.resource(
+          fn -> {ref, task} end,
+          fn {ref, task} ->
+            receive do
+              {^ref, {:data, chunk}} -> {parse_chunk(chunk), {ref, task}}
+              {^ref, :done} -> {:halt, {ref, task}}
+            end
+          end,
+          fn {_ref, task} -> Task.shutdown(task) end
+        )
     )
   end
 
@@ -117,7 +123,7 @@ defmodule LiveLlama.OpenAI do
       json: %{
         model: opts[:model],
         messages: opts[:messages],
-        stream: opts[:stream],
+        stream: opts[:stream]
       },
       auth: {:bearer, opts[:api_key]}
     )
