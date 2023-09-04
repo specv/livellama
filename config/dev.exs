22bfae065883f653f6a71caf5826595127747ac5
specv <61646426+specv@users.noreply.github.com>
2023-09-04 22:05:45 +0800

    Fix the issue of live_reload being triggered twice

--- config/dev.exs
+++ config/dev.exs
@@ -58,7 +58,7 @@ config :livellama, LiveLlamaWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/livellama_web/(controllers|live|components)/.*(ex|heex)$",
-      ~r"lib/livellama/.*$"
+      ~r"lib/livellama/.*(ex)$"
     ]
   ]
 
